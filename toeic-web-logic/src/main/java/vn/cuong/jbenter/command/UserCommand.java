3adede88e3e1c53fdd6a0911323a3a0a96b65e45
root <quoccuongdn96vn@gmail.com>
2019-12-12 10:31:11 +0700

    Commit the file UserCommand.java

---
+++ toeic-web-logic/src/main/java/vn/cuong/jbenter/command/UserCommand.java
@@ -0,0 +1,7 @@
+package vn.cuong.jbenter.command;
+
+import vn.cuong.demo.command.AbstractCommand;
+import vn.cuong.jbenter.core.dto.UserDTO;
+
+public class UserCommand extends AbstractCommand<UserDTO> {
+}
